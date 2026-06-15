7b241c43d276d087bb9d6263e7d53d02aff8fce7
jgarcia691 <jagarciar.info@gmail.com>
2026-06-14 22:53:02 -0400

    ecosystem pm2

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
     {
       name: "sifmo-backend",
       script: "server.js",
-      cwd: "../SIFMO_backend",
+      cwd: "../SIFMO_backend", // Sube un nivel desde frontend y entra a backend
       watch: false,
       env: {
         NODE_ENV: "development"
@@ -11,8 +11,9 @@ module.exports = {
     },
     {
       name: "sifmo-frontend",
-      script: "node_modules/vite/bin/vite.js",
-      cwd: ".",
+      script: "npm",           //En Windows, llamamos a npm para evitar errores con Vite
+      args: "run dev",         // O 'run start' según tengas configurado tu package.json
+      cwd: ".",                // Se ejecuta en la raíz del frontend
       watch: false
     }
   ]
